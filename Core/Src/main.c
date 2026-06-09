f17404b31412ebc86f7c5f8e88ce603198bbedec
Qiyuan Xie <186782911+xquan-xj@users.noreply.github.com>
2026-06-10 00:28:35 +0800

    Configure STM32F103 EIDE development setup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,44 +63,17 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
+    HAL_Init();
 
-  /* USER CODE BEGIN 1 */
+    SystemClock_Config();
 
-  /* USER CODE END 1 */
+    MX_GPIO_Init();
 
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-    HAL_Delay(500);
-  }
-  /* USER CODE END 3 */
+    while (1)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(500);
+    }
 }
 
 /**
